ed43e7f47642027a5cd63c693a68faa9ae75d1c7
Anastasiia <anastasiacapurso11@gmail.com>
2025-02-25 21:58:36 +0100

    Update app files

---
+++ lesson2.js
@@ -0,0 +1,6 @@
+const titleProject = "valuation";
+const screenValue = "template , unique design, animations";
+const screenPrice = 150;
+const percentage = 20;
+const fullPrice = 2000;
+responsive = true;
