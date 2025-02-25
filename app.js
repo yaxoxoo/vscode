0008635add08cca9c79010d8e0874061d5bd1075
Anastasiia <anastasiacapurso11@gmail.com>
2025-02-25 21:11:20 +0100

    Update app files

--- app.js
+++ app.js
@@ -1,8 +1,8 @@
-const firstName = 'Nastya';
-console.log('firstName');
+const firstName = "Nastya";
+console.log(firstName);
 const age = 28;
-console.log('age');
-const job = 'Designer';
-console.log('job');
-const city = 'Barcelona';
-console.log('city');
+console.log(age);
+const job = "Designer";
+console.log(job);
+const city = "Barcelona";
+console.log(city);
