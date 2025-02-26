4a3cead1474f87d4a0d2018748cba91b59c48b1a
Anastasiia <anastasiacapurso11@gmail.com>
2025-02-26 14:43:58 +0100

    Update

--- lesson3/lesson3.js
+++ lesson3/lesson3.js
@@ -1,8 +1,10 @@
 const screenPrice = 150;
-const percentage = 20; 
+const percentage = 20;
 
 const titleProject = prompt("What's the project name?");
-const screensValue = prompt("What are the screens value?: template, unique design, animations");
+const screensValue = prompt(
+  "What are the screens value?: template, unique design, animations"
+);
 const responsive = confirm("Do you need a responsive design?");
 
 const service1 = prompt("What service do you need?");
@@ -13,16 +15,16 @@ const servicePrice2 = Number(prompt("How much will this second service cost?"));
 
 const fullPrice = screenPrice + servicePrice1 + servicePrice2;
 
-const servicePercentPrice = fullPrice * percentage / 100;
+const servicePercentPrice = (fullPrice * percentage) / 100;
 
 if (fullPrice > 50000) {
-    console.log("We will give you a 10% discount.");
+  console.log("We will give you a 10% discount.");
 } else if (fullPrice > 20000 && fullPrice <= 50000) {
-    console.log("We will give you a 5% discount.");
+  console.log("We will give you a 5% discount.");
 } else if (fullPrice > 0 && fullPrice <= 20000) {
-    console.log("No discount is provided.");
+  console.log("No discount is provided.");
 } else if (fullPrice === 0) {
-    console.log("The price is zero. Please check your input.");
+  console.log("The price is zero. Please check your input.");
 } else {
-    console.log("Something went wrong.");
+  console.log("Something went wrong.");
 }
